ef8154351926291d2877616c7f01eedf204a1695
amkabatznick <amkabatznick@gmail.com>
2018-02-20 17:23:46 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var parser = d3.timeParse("%m/%d/%y %H:%M");
+var formatNumber = d3.format(",d");
 
 var barChartClass = barChart()
     .x(function(d) {return d.key;})
@@ -28,13 +29,19 @@ d3.csv("https://raw.githubusercontent.com/amkabatznick/W209/master/AMK_Peloton.c
     csData.classTypes = csData.dimClassType.group().reduceSum(function(d) { return d.avg_watts; });
 
 
+    d3.selectAll("#total")
+    .text(formatNumber(csData.size()));
+
+    d3.select("#active").text(formatNumber(csData.dimWorkouts.groupAll().value()));
 
     barChartClass.onMouseOver(function (d) {
       csData.dimClassType.filter(d.key);
+      d3.select("#active").text(formatNumber(csData.dimWorkouts.groupAll().value()));
       update();
     }).onMouseOut(function () {
       // Clear the filter
       csData.dimClassType.filterAll();
+      d3.select("#active").text(formatNumber(csData.dimWorkouts.groupAll().value()));
       update();
     });
 
@@ -49,7 +56,6 @@ d3.csv("https://raw.githubusercontent.com/amkabatznick/W209/master/AMK_Peloton.c
         .select(".x.axis") //Adjusting the tick labels after drawn
         .selectAll(".tick text")
         .attr("transform", "translate(-8,-1) rotate(-45)");
-
     }
 
     update();
